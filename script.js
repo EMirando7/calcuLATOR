f5a4383eefa06359b04997bf59e7f0b1c9eb356c
Emiliano Miranda <emirando117@gmail.com>
2020-12-31 02:21:14 -0600

    complete base game

--- script.js
+++ script.js
@@ -1,13 +1,105 @@
 const body = document.querySelector('body');
 const container = document.querySelector('#container');
+const inputCell = document.createElement('input');
+const child = container.children;
+let firstBatchHolder = [];
+let secondBatchHolder = [];
+let holder;
 
-const addCells = () =>{
+const addCells = function(){
     const lastCell = document.querySelector('#lastCell') 
-    for(let i = 0 ; i < 18; i ++){
+    for(let i = 0 ; i < 18; i ++){ //22 is to include input space
         let cell = document.createElement('div');
         container.insertBefore(cell, lastCell );
+        if(i === 3 || i === 7 || i === 11 || i === 15){
+            container.children[i].classList.add('orangeC')
+        }
+        lastCell.classList.add('orangeC')
         container.children[i].classList.add(`cell${i}`)
+        container.children[i].textContent = `${i}`
+    }
+    //insert a div inside body before grid 
+    body.insertBefore(inputCell, container);
+    // select and add class
+    let input = document.querySelector('input');
+    input.classList.add('inputCell');
+    input.setAttribute('placeholder', '0');
+    //prevents adding more than 9 with keyboard
+    input.setAttribute('onkeydown', 'limit(this)');
+    input.setAttribute('onkeyup', 'limit(this)')
+    addTextAndListeners();
+
+}
+const addTextAndListeners =  function(){
+    const inputField = document.querySelector('input');
+    inputCell.setAttribute('max', '444');
+    child[0].textContent = 'AC';
+    child[1].textContent = '+/-';
+    child[2].textContent = '%';
+    child[3].textContent = '/';
+    child[4].textContent = 7;
+    child[5].textContent = 8;
+    child[6].textContent = 9;
+    child[7].textContent = 'x';
+    child[8].textContent = '4';
+    child[9].textContent = 5;
+    child[10].textContent = 6;
+    child[11].textContent = '-';
+    child[12].textContent = 3;
+    child[13].textContent = 2;
+    child[14].textContent = 1;
+    child[15].textContent = '+';
+    child[16].textContent = '.';
+    child[17].textContent = 0;
+    child[18].textContent = '=';
+    for(let i = 0 ; i < 19; i ++){
+        if(child[i].textContent < 11 && child[i].textContent != 0){
+            child[i].addEventListener('click', function() {
+                        // limits clicked in input
+                if([...inputCell.value].length < 10){
+                    inputCell.value += child[i].textContent;
+                }
+                if(holder.length > 0 && [...inputCell.value].length < 10){
+                    inputCell.value = +holder;
+                }  
+            })            
+        }
+                //clicking on zero with more than 1 length
+        if(child[i].textContent == 0){
+            child[i].addEventListener('click', () => {
+                if([...inputField.value].length >=1 && [...inputField.value].length <= 9){
+                    inputField.value += child[i].textContent;
+                }
+            })  
+        }
+    }
+
+    child[15].addEventListener('click', function hello(){
+        if(firstBatchHolder.length < 1 && secondBatchHolder.length < 1){
+            holder = [];
+            firstBatchHolder += inputCell.value;
+            holder = inputCell.value;
+
+            
+        }
+        else if(firstBatchHolder.length >= 1 && secondBatchHolder.length < 1){
+            inputCell.value = (+firstBatchHolder) + (+secondBatchHolder)
+        }
+        else{
+            inputCell.value = (+firstBatchHolder) + (+secondBatchHolder)
+            console.log('hi')
+        }
+    })
+}
+
+
+
+// ########KEYBOARD LIMIT INPUT 
+const limit = element => {
+    var max_chars = 9   ;
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
     }
 }
 
-addCells()
+addCells();
---
+++ testscript.js
@@ -0,0 +1,203 @@
+const container = document.querySelector("#container");
+const input = document.querySelector("input");
+const inputSoFarSelector = document.querySelector('#inputSoFar');
+addCellNumClasses(),workingNumberCells(),plusOrMinus(),showPercentage(),Add(),Division(),Multiply(),Subtract()
+function showPercentage(){
+    container.children[2].addEventListener('click', function(){
+        input.value = input.value * 0.01
+    }, false)
+}
+function addCellNumClasses() {
+    let oneToTenBtns = [1,2,3,4,5,6,7,8,9,0,'.'];
+    for(let i = 0 ; i < 19; i ++){
+        container.children[i].classList.add('cell')
+        if(oneToTenBtns.includes(+container.children[i].textContent)) container.children[i].classList.add('number');
+    }
+    container.children[17].classList.add('number');
+}
+function workingNumberCells () {
+    document.addEventListener('click', function(e){
+    clearInputACBtn();
+    if(input.value.length <= 8){
+        if(e.target.classList.contains('number') && !e.target.classList.contains('dot') && !e.target.classList.contains('cell0')){
+            input.value += e.target.textContent
+        }
+        if(e.target.classList.contains('dot') && !input.value.includes('.')){
+            input.value += e.target.textContent
+        }
+        if(e.target.classList.contains('cell0') && !input.value[0] == '0'){
+            input.value += e.target.textContent
+        }
+    }
+    removeHyph();
+    }, false)
+}
+function removeHyph(){
+    return input.value.length == 1 && input.value === '-' ? input.value = '' : input.value;
+}
+function clearInputACBtn (){
+    container.children[0].addEventListener('click', function(){
+        input.value = '';
+        inputSoFarSelector.innerHTML = '0'
+    }, false)
+}
+function plusOrMinus(){
+    container.children[1].addEventListener('click', function(){
+        let tempArr = [...input.value];
+        if(input.value.includes('-')){
+            tempArr.shift()
+            tempArr = tempArr.join('');
+            input.value = tempArr
+        } else {
+            tempArr.unshift('-');
+            tempArr =tempArr.join('');
+            input.value = tempArr
+        }
+    }, false)
+}
+function Division(){
+    let firstNum = '';
+    let secondNum = '';
+    let divisionEventListenerFunc = function(){
+        inputSoFarSelector.innerHTML = input.value;
+        firstNum = +input.value;
+        input.value = ''
+        if(!inputSoFarSelector.innerHTML.includes('/')) inputSoFarSelector.innerHTML += ' ' + '/'
+        input.value = '';
+    }
+    container.children[3].addEventListener('click', divisionEventListenerFunc, false)
+    container.children[18].addEventListener('click', () => {
+        if(inputSoFarSelector.innerHTML.includes('*') || inputSoFarSelector.innerHTML.includes('-') || inputSoFarSelector.innerHTML.includes('+')){
+            if(inputSoFarSelector.innerHTML.indexOf('-') == 0 && !inputSoFarSelector.innerHTML.includes('*')&& !inputSoFarSelector.innerHTML.includes('-')){
+                secondNum = +input.value;
+                input.value = (firstNum / secondNum);
+                return
+            }
+            else if(inputSoFarSelector.innerHTML.indexOf('-') == 0 && !inputSoFarSelector.innerHTML.includes('*')){
+                if(inputSoFarSelector.innerHTML.indexOf('-',1) !== 0 && !inputSoFarSelector.innerHTML.includes('/')) return
+                else if(inputSoFarSelector.innerHTML.includes('/')){
+                    secondNum = +input.value;
+                    input.value = (firstNum / secondNum);
+                    return;
+                }
+                secondNum = +input.value;
+                input.value = (firstNum / secondNum)
+                return;
+            }
+            else{
+                return
+            }
+        }
+        inputSoFarSelector.innerHTML += ' ' + input.value;
+        secondNum = +input.value;
+        input.value = (firstNum / secondNum)
+    }, false)
+}
+function Multiply(){
+    let firstNum = '';
+    let secondNum = '';
+    let multiplyEventListenerFunc = function(){
+        inputSoFarSelector.innerHTML = input.value;
+        firstNum = +input.value;
+        input.value = ''
+        if(!inputSoFarSelector.innerHTML.includes('*')) inputSoFarSelector.innerHTML += ' ' + '*'
+        input.value = '';
+    }
+    container.children[7].addEventListener('click', multiplyEventListenerFunc, false)
+    container.children[18].addEventListener('click', () =>{
+        //takes out control of function if there was a previously used armithemic 
+        if(inputSoFarSelector.innerHTML.includes('/') || inputSoFarSelector.innerHTML.includes('-') ||inputSoFarSelector.innerHTML.includes('+')){
+            if(inputSoFarSelector.innerHTML.indexOf('-') == 0 && !inputSoFarSelector.innerHTML.includes('/') && !inputSoFarSelector.innerHTML.includes('+') && !inputSoFarSelector.innerHTML.includes('-')){  
+                // inputSoFarSelector.innerHTML += ' ' + input.value;
+                secondNum = +input.value;
+                input.value = (firstNum * secondNum);
+                return;
+            }
+            else if(inputSoFarSelector.innerHTML.indexOf('-') == 0 && inputSoFarSelector.innerHTML.includes('*')){
+                secondNum = +input.value;
+                input.value = (firstNum * secondNum);
+                return
+            }else{
+                return
+            }
+        }
+        inputSoFarSelector.innerHTML += ' ' + input.value;
+        secondNum = +input.value;
+        input.value = (firstNum * secondNum);
+    }, false)
+}
+function Subtract(){
+    let firstNum = '';
+    let secondNum = '';
+    let subtractEventListenerFunc = function(){
+        inputSoFarSelector.innerHTML = input.value;
+        firstNum = +input.value;
+        input.value = ''
+        if(!inputSoFarSelector.innerHTML.includes('*')) inputSoFarSelector.innerHTML += ' ' + '-'
+        input.value = '';
+    }
+    container.children[11].addEventListener('click', subtractEventListenerFunc, false)
+    container.children[18].addEventListener('click', () =>{
+        if(inputSoFarSelector.innerHTML.includes('/') || inputSoFarSelector.innerHTML.includes('*') || inputSoFarSelector.innerHTML.includes('+')|| inputSoFarSelector.innerHTML.includes('-')){
+            if(inputSoFarSelector.innerHTML.indexOf('-') == 0 && !inputSoFarSelector.innerHTML.includes('/') && !inputSoFarSelector.innerHTML.includes('+') && !inputSoFarSelector.innerHTML.includes('-')){
+                secondNum = +input.value;
+                input.value = (firstNum - secondNum);
+                return;
+            } else if(inputSoFarSelector.innerHTML.indexOf('-') == 0 && !inputSoFarSelector.innerHTML.includes('/') &&inputSoFarSelector.innerHTML.includes('*')&& inputSoFarSelector.innerHTML.includes('+')){
+                // inputSoFarSelector.innerHTML += ' ' + input.value;
+                secondNum = +input.value;
+                input.value = (firstNum - secondNum)
+                return;
+            }
+            else if(inputSoFarSelector.innerHTML.includes('-')&& !inputSoFarSelector.innerHTML.includes('/') && !inputSoFarSelector.innerHTML.includes('*') && !inputSoFarSelector.innerHTML.includes('+')){
+                if(!inputSoFarSelector.innerHTML.indexOf('-') ==0 && inputSoFarSelector.innerHTML.indexOf('-', 1)) {
+                    inputSoFarSelector.innerHTML += ' ' + input.value;
+                    secondNum = +input.value;
+                    input.value = (firstNum - secondNum)
+                    return
+                }
+                if(inputSoFarSelector.innerHTML.indexOf('-')==0 && inputSoFarSelector.innerHTML.indexOf('-', 1)){
+                    secondNum = +input.value;
+                    input.value = (firstNum - secondNum)
+                    return
+                } 
+                inputSoFarSelector.innerHTML += ' ' + input.value;
+                secondNum = +input.value;
+                input.value = (firstNum - secondNum)
+                return;
+            }else {
+                return
+            }
+        }
+        inputSoFarSelector.innerHTML += ' ' + input.value;
+        secondNum = +input.value;
+        input.value = (firstNum - secondNum)
+    }, false)
+}
+function Add(){
+    let firstNum = '';
+    let secondNum = '';
+    let additionEventListenerFunc = function(){
+        inputSoFarSelector.innerHTML = input.value;
+        firstNum = +input.value; //8
+        input.value = ''
+        if(!inputSoFarSelector.innerHTML.includes('+')) inputSoFarSelector.innerHTML += ' ' + '+'
+        input.value = '';
+    }
+    container.children[15].addEventListener('click', additionEventListenerFunc, false)
+    container.children[18].addEventListener('click', () =>{
+        if(inputSoFarSelector.innerHTML.includes('-') || inputSoFarSelector.innerHTML.includes('*') || inputSoFarSelector.innerHTML.includes('/')){
+            if(inputSoFarSelector.innerHTML.indexOf('-') == 0){
+                inputSoFarSelector.innerHTML += ' ' + input.value;
+                secondNum = +input.value;
+                input.value = (firstNum + secondNum);
+                return;
+            } else{
+                return;
+            }
+        }
+        inputSoFarSelector.innerHTML += ' ' + input.value;
+        secondNum = +input.value;
+        input.value = (firstNum + secondNum)
+    }, false)
+}
